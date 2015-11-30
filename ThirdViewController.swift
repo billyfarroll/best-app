7232edcba93a2d0874dd135e2e5dc3797a291bbf
Billy Farroll <watanight@hotmail.co.uk>
2015-11-30 20:46:02 +0000

    Update ThirdViewController.swift

--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -5,6 +5,8 @@ import CoreLocation
 class ThirdViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var bournemouthMap: MKMapView!
+    
+    let locationManager = CLLocationManager()
 
     @IBAction func segmentControl(sender: UISegmentedControl) {
        
@@ -35,19 +37,53 @@ class ThirdViewController: UIViewController, CLLocationManagerDelegate {
         
         
         let chapelGate = CLLocationCoordinate2D(latitude: 50.7823501505709 , longitude: -1.85909584510079 )
-        
-        let chapelGateRegion = CLCircularRegion(center: chapelGate, radius: 50, identifier: "Chapel-Gate")
-        
+        let chapelGateRegion = CLCircularRegion(center: chapelGate, radius: 50, identifier: "Chapel Gate")
+        locationManager.startMonitoringForRegion(chapelGateRegion)
+
         let bournemouthAthleticClub = CLLocationCoordinate2D(latitude: 50.7345638 , longitude: -1.8422774 )
-        
-        let bournemouthAthleticClubRegion = CLCircularRegion(center: bournemouthAthleticClub, radius: 50, identifier: "Bournemouth Athletic Club")
+        let bournemouthAthleticClubRegion = CLCircularRegion(center: bournemouthAthleticClub, radius: 50, identifier: "Athletic club")
+        locationManager.startMonitoringForRegion(bournemouthAthleticClubRegion)
         
         let argyllBowlingClub = CLLocationCoordinate2D(latitude: 50.7132 , longitude: -1.89552 )
-        
         let argyllBowlingClubRegion = CLCircularRegion(center: argyllBowlingClub, radius: 50, identifier: "Argyll Bowling Club")
-        
+        locationManager.startMonitoringForRegion(argyllBowlingClubRegion)
         
         
     }
 
+
+
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        
+        
+        if region.identifier == "Chapel Gate" {
+            
+            let refreshAlert = UIAlertController(title: region.identifier, message: "You've found Chapel Gate ", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            refreshAlert.addAction(UIAlertAction(title: "More Info", style: .Default, handler: { (action: UIAlertAction!) in
+                self.performSegueWithIdentifier("Next", sender: self)
+                
+            }))
+            
+            refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .Default, handler: { (action: UIAlertAction!) in
+                
+                refreshAlert .dismissViewControllerAnimated(true, completion: nil)
+                
+                
+            }))
+            
+            presentViewController(refreshAlert, animated: true, completion: nil)
+            
+        }
+        
+        
+        
+    }
+    
+
+
+
+
 }
+
+
