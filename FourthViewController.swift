80a13560a680277c913d933c5aafb6c9940b91d9
Billy Farroll <watanight@hotmail.co.uk>
2015-12-01 11:04:09 +0000

    Update FourthViewController.swift

--- FourthViewController.swift
+++ FourthViewController.swift
@@ -4,6 +4,28 @@ import UIKit
 class FourthViewController: UIViewController {
 
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
 
+        imageView.animationImages = [
+            
+            UIImage(named: "Athletic logo"),
+            UIImage(named: "Bournemouth Athletics"),
+            UIImage(named: "Bournemouth Athletic's2")
+            
+        ]
+        
+        imageView.animationDuration = 4
+        imageView.startAnimating()
+
+        
+    
+    }
+    
 
 }
