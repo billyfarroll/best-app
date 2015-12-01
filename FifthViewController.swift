ed22ae95ae4c4b7c20af3efdd86e32ccfa3d0f6b
Billy Farroll <watanight@hotmail.co.uk>
2015-12-01 11:04:30 +0000

    Update FifthViewController.swift

--- FifthViewController.swift
+++ FifthViewController.swift
@@ -4,6 +4,27 @@ import UIKit
 class FifthViewController: UIViewController {
     
     
+    @IBOutlet weak var imageView: UIImageView!
     
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        imageView.animationImages = [
+            
+            UIImage(named: "chapel gate1"),
+            UIImage(named: "chapel gate2"),
+            UIImage(named: "chapel gate3")
+            
+        ]
+        
+        imageView.animationDuration = 4
+        imageView.startAnimating()
+        
+        
+        
+    }
+
     
 }
