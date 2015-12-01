044160c72c06ae50b5d31017e0fd013b67133802
Billy Farroll <watanight@hotmail.co.uk>
2015-12-01 11:32:48 +0000

    Update FourthViewController.swift

--- FourthViewController.swift
+++ FourthViewController.swift
@@ -13,14 +13,17 @@ class FourthViewController: UIViewController {
     
 
         imageView.animationImages = [
-            
+
             UIImage(named: "Athletic logo")!,
             UIImage(named: "Bournemouth Athletics")!,
-            UIImage(named: "Bournemouth Athletic's2")!
+            UIImage(named: "Bournemouth Athletic's2")!,
+            UIImage(named: "you will")!,
+            UIImage(named: "Have a")!,
+            UIImage(named: "Great time")!
             
         ]
         
-        imageView.animationDuration = 4
+        imageView.animationDuration = 6
         imageView.startAnimating()
 
         
