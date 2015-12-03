3430270b7f01671c4617deb44b869622ee95aff3
Billy Farroll <watanight@hotmail.co.uk>
2015-12-03 23:33:14 +0000

    Create WebViewController.swift

---
+++ WebViewController.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        }
+    
+    @IBAction func didGobutton(sender: AnyObject) {
+        
+        if textField.text == "" {
+        return
+        
+    }
+        guard let text: String = textField.text else {
+        
+        return
+        }
+        
+        infoLabel.hidden = true
+        
+        if text.rangeOfString(".") != nil {
+            var finalString :String = text.lowercaseString
+            if finalString.rangeOfString("http://") == nil {
+             finalString = "http://\(finalString)"
+            
+            }
+        self.loadURL(finalString)
+        } else {
+        
+        let searchString = text.stringByReplacingOccurrencesOfString(" ", withString: "+")
+        let finalString = "https://www.google.com/search?q=\(searchString)"
+        self.loadURL(finalString)
+        }
+    
+    }
+    
+    
+    func loadURL(str:String) {
+    
+    let url = NSURL(string: str)!
+    let request = NSURLRequest(URL: url)
+    webView.loadRequest(request)
+    
+    }
+    
+    
+    @IBAction func goBack(sender: AnyObject) {
+        webView.goBack()
+    }
+    
+
+    @IBAction func goForward(sender: AnyObject) {
+        webView.goForward()
+    }
+    
+}
